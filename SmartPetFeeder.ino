704a2925aad81f548f2615b3ac6f227ae0abee61
Michael Oliva Tonilon <130451107+MTonilon22@users.noreply.github.com>
2024-05-14 01:03:50 +0800

    Updated
    Updated

--- SmartPetFeeder.ino
+++ SmartPetFeeder.ino
@@ -49,58 +49,65 @@ String getPageContent() {
   String content = "<!DOCTYPE html><html>";
   content += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
   content += "<link rel=\"icon\" href=\"data:,\">";
-  content += "<style>html { font-family: monospace; display: inline-block; margin: 0px auto; text-align: center;}";
-  content += ".button { background-color: yellowgreen; border: none; color: white; padding: 16px 40px;";
-  content += "text-decoration: none; font-size: 32px; margin: 2px; cursor: pointer;}";
-  content += ".button2 {background-color: gray;}</style></head>";
-  content += "<body><h1>ESP32 Web Server</h1>";
-  content += "<p>Control Smart Pet Feeder</p>";
+  content += "<style>html { font-family: Arial, sans-serif; text-align: center;}";
+  content += "input[type='number'] { padding: 10px; margin: 10px; width: 60px;}";
+  content += ".button { border-radius: 10px; padding: 10px 20px; margin: 10px; cursor: pointer; font-size: 18px;}";
+  content += "#startButton { background-color: #6fe1bc; border: 2px solid #6fe1bc; color: white;}";
+  content += "#stopButton { background-color: #febb00; border: 2px solid #febb00; color: white;}";
+  content += "#resetButton { background-color: #ff0037; border: 2px solid #ff0037; color: white;}";
+  content += "#quickFeedButton { background-color: #2596be; border: 2px solid #2596be; color: white;}";
+  content += ".button:hover { opacity: 0.8;}";
+  content += "</style></head>";
+  content += "<body>";
+  content += "<h1>Control Smart Pet Feeder</h1>";
+  content += "<button id=\"quickFeedButton\" class=\"button\" onclick=\"quickFeed()\">Quick Feed</button>";
+  content += "<br><br>";
   content += "<label for=\"hours\">Hours:</label>";
   content += "<input type=\"number\" id=\"hours\" name=\"hours\" min=\"0\" max=\"23\">";
   content += "<label for=\"minutes\">Minutes:</label>";
   content += "<input type=\"number\" id=\"minutes\" name=\"minutes\" min=\"0\" max=\"59\">";
   content += "<label for=\"seconds\">Seconds:</label>";
   content += "<input type=\"number\" id=\"seconds\" name=\"seconds\" min=\"0\" max=\"59\">";
+  content += "<br><br>";
   content += "<button id=\"startButton\" class=\"button\" onclick=\"startTimer()\">Start Timer</button>";
-  content += "<button id=\"stopButton\" class=\"button\" onclick=\"stopTimer()\" disabled>Stop Timer</button>";
+  content += "<button id=\"stopButton\" class=\"button\" onclick=\"stopTimer()\">Stop Timer</button>";
   content += "<button id=\"resetButton\" class=\"button\" onclick=\"resetTimer()\">Reset Timer</button>";
+  content += "<div id=\"timerDisplay\"></div>";
   content += "<script>";
   content += "var timer;";
-
-  //Start Timer Function
+  content += "var remainingSeconds = 0;"; // Variable to store remaining seconds
+  
+  // Start Timer Function
   content += "function startTimer() {";
   content += "var hours = document.getElementById('hours').value;";
   content += "var minutes = document.getElementById('minutes').value;";
   content += "var seconds = document.getElementById('seconds').value;";
-  content += "var totalSeconds = (hours * 3600) + (minutes * 60) + parseInt(seconds);";
-  content += "if (totalSeconds > 0) {";
+  content += "remainingSeconds = (hours * 3600) + (minutes * 60) + parseInt(seconds);"; // Initialize remainingSeconds
+  content += "if (remainingSeconds > 0) {";
   content += "document.getElementById('startButton').disabled = true;";
   content += "document.getElementById('hours').disabled = true;";
   content += "document.getElementById('minutes').disabled = true;";
   content += "document.getElementById('seconds').disabled = true;";
   content += "timer = setInterval(function() {";
-  content += "totalSeconds--;";
-  content += "var hoursLeft = Math.floor(totalSeconds / 3600);";
-  content += "var minutesLeft = Math.floor((totalSeconds % 3600) / 60);";
-  content += "var secondsLeft = totalSeconds % 60;";
+  content += "remainingSeconds--;";
+  content += "var hoursLeft = Math.floor(remainingSeconds / 3600);";
+  content += "var minutesLeft = Math.floor((remainingSeconds % 3600) / 60);";
+  content += "var secondsLeft = remainingSeconds % 60;";
   content += "document.getElementById('timerDisplay').innerHTML = hoursLeft + 'h ' + minutesLeft + 'm ' + secondsLeft + 's';";
-  content += "if (totalSeconds <= 0) {";
+  content += "if (remainingSeconds <= 0) {";
   content += "clearInterval(timer);";
   content += "rotateServo();";
   content += "document.getElementById('hours').disabled = false;";
   content += "document.getElementById('minutes').disabled = false;";
-   content += "document.getElementById('seconds').disabled = false;";
-   
-   content += "document.getElementById('startButton').disabled = false;";
-   content += "resetTimer()";
+  content += "document.getElementById('seconds').disabled = false;";
+  content += "document.getElementById('startButton').disabled = false;";
+  content += "resetTimer();";
   content += "}";
   content += "}, 1000);";
   content += "} ";
   content += "}";
 
- 
- 
-  //Stop Timer Function
+  // Stop Timer Function
   content += "function stopTimer() {";
   content += "clearInterval(timer);";
   content += "document.getElementById('startButton').disabled = false;";
@@ -109,19 +116,28 @@ String getPageContent() {
   content += "document.getElementById('seconds').disabled = false;";
   content += "}";
 
-  //Reset Timer Function
+  // Reset Timer Function
   content += "function resetTimer() {";
   content += "clearInterval(timer);";
   content += "document.getElementById('startButton').disabled = false;";
   content += "document.getElementById('hours').value = '';";
   content += "document.getElementById('minutes').value = '';";
   content += "document.getElementById('seconds').value = '';";
- 
   content += "document.getElementById('hours').disabled = false;";
   content += "document.getElementById('minutes').disabled = false;";
   content += "document.getElementById('seconds').disabled = false;";
   content += "document.getElementById('timerDisplay').innerHTML = '';";
   content += "}";
+  
+  // Quick Feed Function
+  content += "function quickFeed() {";
+  content += "document.getElementById('quickFeedButton').disabled = true;"; // Disable the button
+  content += "rotateServo();"; // Call the rotate servo function
+  content += "setTimeout(function() {"; // Set a timeout to enable the button after 10 seconds
+  content += "document.getElementById('quickFeedButton').disabled = false;";
+  content += "}, 10000);"; // 10 seconds
+  content += "}";
+  
   content += "function rotateServo() {";
   content += "var xhr = new XMLHttpRequest();";
   content += "xhr.open('GET', '/rotate', true);";
@@ -135,7 +151,7 @@ String getPageContent() {
 
 
 void rotateServo() {
-  myservo.write(180); // Rotate servo to 180 degrees
-  delay(1000); // Wait for 1 second
-  myservo.write(0); // Rotate servo back to 0 degrees
+  myservo.write(0); // Rotate servo to 180 degrees
+  delay(100); // Wait for 1 second
+  myservo.write(90); // Rotate servo back to 0 degrees
 }
